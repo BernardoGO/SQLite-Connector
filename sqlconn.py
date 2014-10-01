ea49623af645d559c76dc029bde71539754bbcfc
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2014-10-01 20:04:11 -0300

    sqlconn.py

--- sqlconn.py
+++ sqlconn.py
@@ -2,13 +2,24 @@ __author__ = 'bernardo'
 
 import sqlite3 as lite
 
-con = lite.connect('test.db')
-con.row_factory = lite.Row
 
+
+con = None
+
+def connect(filename):
+    global con
+    con = lite.connect(filename)
+    con.row_factory = lite.Row
+
+def readTable(tableName, where = ""):
 
 
 class entity:
     pass
+
+
+
+
 """
 aa.uu = 11
 vars(aa)['ca'] = 332
@@ -16,4 +27,3 @@ vars(aa)['ca'] = 332
 print vars(aa)
 """
 if __name__ == "__main__":
-    print 4543543

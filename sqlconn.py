39c176f328774006ae5f1e180b0ba8cf11d43dd3
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2014-10-01 20:00:48 -0300

    sqlconn.py

--- sqlconn.py
+++ sqlconn.py
@@ -1,10 +1,17 @@
 __author__ = 'bernardo'
 
+import sqlite3 as lite
 
-class aa:
-    bb = 32
+con = lite.connect('test.db')
+con.row_factory = lite.Row
+
+
+
+class entity:
+    pass
 
 aa.uu = 11
 vars(aa)['ca'] = 332
 
 print vars(aa)
+
